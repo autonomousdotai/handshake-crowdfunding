a613640c434af3b2c669414f6b164ef7e19256d4
hieuqautonomous <hieu.q@autonomous.nyc>
2018-05-25 17:08:52 +0700

    update package sh

---
+++ crowdfunding-service/deploy/package.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+go get github.com/gin-gonic/gin
+go get github.com/jinzhu/gorm
+go get github.com/go-sql-driver/mysql
+go get github.com/gin-contrib/cors
+go get github.com/gin-gonic/contrib/sessions
+go get cloud.google.com/go/storage
